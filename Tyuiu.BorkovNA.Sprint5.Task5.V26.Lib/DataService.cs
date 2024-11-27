bffbae0ebc1ac3c4a4d3931e39248b589265aa00
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-11-27 21:05:16 +0500

    5

---
+++ Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib/DataService.cs
@@ -0,0 +1,43 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+using System.Text;
+
+
+namespace Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib
+{
+    public class DataService : ISprint5Task5V26
+    {
+        public double LoadFromDataFile(string path)
+        {
+            double res = 0;
+            double res1 = 0;
+            double res2 = 0;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] str = line.Split(',');
+                    foreach (string str2 in str)
+                    {
+                        if (Convert.ToDouble(str2) < 0)
+                        {
+                            string str3 = str2.Replace(".", ",");
+                            res1 = res1 + Convert.ToDouble(str2);
+                        }
+
+                        if (Convert.ToDouble(str2) > 0)
+                        {
+                            string str3 = str2.Replace(".", ",");
+                            res2 = res2 + Convert.ToDouble(str2);
+                        }
+                        res = res2 - res1;
+                    }
+                }
+            }
+            res = Math.Round(res, 3);
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.BorkovNA.Sprint5.Task5.V26/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib;
+
+
+
+
+namespace Tyuiu.BorkovNA.Sprint5.Task26.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Cпринт #5 | Выполнил: Борков Н. А. | пктб-24-1";
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* Спринт #5                                                                *");
+            Console.WriteLine("* Тема: Операции сравнения                                                 *");
+            Console.WriteLine("* Задание #5                                                               *");
+            Console.WriteLine("* Вариант #26                                                              *");
+            Console.WriteLine("* Выполнил: Борков Н. А. | пктб-24-1                                       *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                 *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("*                                                                          *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("****************************************************************************");
+            string path = @"C:\DataSprint5\InPutDataFileTask5V26.txt";
+            Console.WriteLine("* Путь: " + path + " *");
+            Console.WriteLine("****************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("****************************************************************************");
+            double res = ds.LoadFromDataFile(Path.Combine("C:", "DataSprint5", "InPutDataFileTask5V26.txt"));
+            Console.WriteLine("Файл" + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+
+        }
+    }
+
+}
