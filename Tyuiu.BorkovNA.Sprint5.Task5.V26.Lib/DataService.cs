4efcb9587fdfe8dc82a2dc3f4939bcce5e729a91
Nikolay Borkov <ghost0dragon13@gmail.com>
2024-11-27 21:19:22 +0500

    5

--- Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib/DataService.cs
+++ Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib/DataService.cs
@@ -10,6 +10,8 @@ namespace Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib
         public double LoadFromDataFile(string path)
         {
             double res = 0;
+            double res1 = 0;
+            double res3 = 0;
 
             using (StreamReader sr = new StreamReader(path))
             {
@@ -19,16 +21,22 @@ namespace Tyuiu.BorkovNA.Sprint5.Task5.V26.Lib
                     string[] str = line.Split(',');
                     foreach (string str2 in str)
                     {
-                        if (Convert.ToDouble(str2) % 2 == 0)
+                        if (Convert.ToDouble(str2) > 0)
                         {
                             string str3 = str2.Replace(".", ",");
                             res = res + Convert.ToDouble(str2);
                         }
+                        if (Convert.ToDouble(str2) < 0)
+                        {
+                            string str3 = str2.Replace(".", ",");
+                            res1 = res1 + Convert.ToDouble(str2);
+                        }
+                        res3 = res - res1;
                     }
                 }
             }
-            res = Math.Round(res,3);
-            return res;
+            res3 = Math.Round(res3,3);
+            return res3;
         }
     }
 }
